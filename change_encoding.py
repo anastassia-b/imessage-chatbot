5a83d84ae003dd353ec0f243276b4a1cecaf499a
Anastassia Bobokalonova <email.anastassia@gmail.com>
2018-02-20 19:22:10 -0800

    Encode chats as ascii text

---
+++ change_encoding.py
@@ -0,0 +1,9 @@
+
+with open("./all_the_chats.txt", "r") as chat_file:
+    chat_text = chat_file.read()
+    # utf8_chat_text = chat_text.decode("utf-8")
+    ascii_bytes = chat_text.encode("ascii","ignore")
+    ascii_text = ascii_bytes.decode()
+
+with open("./ascii_chats.txt", "w") as ascii_file:
+    ascii_file.write(ascii_text)
