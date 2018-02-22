4b7d6236dd7915784e43337c728b3052e5f9750d
Anastassia Bobokalonova <email.anastassia@gmail.com>
2018-02-21 22:28:00 -0800

    Add generator!!!

---
+++ generator.py
@@ -0,0 +1,44 @@
+from model import build_graph
+import numpy as np
+import tensorflow as tf
+from config import *
+from dataset import batches
+
+session = tf.InteractiveSession()
+#need to build the graph before you restore the variables so they have a home
+
+graph = build_graph(1)
+
+# Lets restore the model
+saver = tf.train.Saver()
+saver.restore(session, "./checkpoints/model-5")
+
+text = ""
+initial_state1 = np.zeros([1, NUM_STATE1_UNITS])
+initial_char = np.zeros([1, NUM_CHARS])
+
+def generate_char():
+    global initial_state1, initial_char, text #not javascript
+
+    result = session.run({
+                "final_state": graph["final_state"],
+                "final_probabilities": graph["final_probabilities"]
+                },
+                feed_dict={
+                    graph["initial_state1"]: initial_state1,
+                    graph["initial_char"]: initial_char})
+
+    initial_state1 = result["final_state"]
+    # selected_char = chr(np.argmax(result["final_probabilities"]))
+    selected_char = chr(np.random.choice(NUM_CHARS, p=(result["final_probabilities"])[0, :]))
+    initial_char = np.zeros([1, NUM_CHARS])
+    initial_char[0, ord(selected_char)] = 1
+    text += selected_char
+
+for i in range(1024*4):
+    generate_char()
+
+with open("./results/generated_text.txt", "w") as generated_file:
+    generated_file.write(text)
+
+print(text)
