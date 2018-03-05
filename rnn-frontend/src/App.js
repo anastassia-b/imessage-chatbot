3bdea3dd56d8801c97b4ac855c4805f377e531d6
Anastassia Bobokalonova <email.anastassia@gmail.com>
2018-03-04 20:02:49 -0800

    Add Chat component

--- rnn-frontend/src/App.js
+++ rnn-frontend/src/App.js
@@ -1,16 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
+import Chat from './components/Chat.js';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <main>
+          <Chat />
+        </main>
       </div>
     );
   }
---
+++ rnn-frontend/src/components/Chat.js
@@ -0,0 +1,48 @@
+import React from 'react';
+
+class Chat extends React.Component {
+  constructor(props) {
+    super(props);
+    this.getMessage = this.getMessage.bind(this);
+    this.messageList = this.messageList.bind(this);
+    this.state = { 'messages': [] };
+  }
+
+  // async getMessage() {
+  //   const response = await fetch('http://ec2-52-42-96-48.us-west-2.compute.amazonaws.com/message');
+  //   const message = await response.text();
+  //   await this.setState((prevState, props) => {
+  //     prevState['messages'].push(message);
+  //     return { 'messages': prevState['messages']}
+  //   });
+  // }
+
+  async getMessage() {
+    const response = await fetch('http://numbersapi.com/random/math')
+    const message = await response.text();
+    await this.setState((prevState, props) => {
+      prevState['messages'].push(message);
+      return { 'messages': prevState['messages']}
+    });
+  }
+
+  messageList() {
+    return this.state.messages.map(message => (
+      <li>{message}</li>
+    ))
+  }
+
+  render() {
+    return (
+      <div className="Chat">
+        <h2>Chat Log goes here</h2>
+        <button onClick={this.getMessage}>Click to get a message</button>
+        <ul>
+          {this.messageList()}
+        </ul>
+      </div>
+  );
+  }
+}
+
+export default Chat;
