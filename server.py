5429bf135d629a6ced3ac4c325fbf55fed36a932
Anastassia Bobokalonova <email.anastassia@gmail.com>
2018-03-14 16:48:42 -0700

    Update /sms route

--- server.py
+++ server.py
@@ -10,11 +10,9 @@ CORS(app)
 # decorator, wraps the function
 @app.route("/sms", methods=['GET', 'POST'])
 def sms_reply():
-    """Respond to incoming calls with a simple text message."""
-    # Start our TwiML response
     resp = MessagingResponse()
-    ron_message = generate_message(459)
-    resp.message(ron_message)
+    rnn_message = generate_message(459)
+    resp.message(rnn_message)
 
     return str(resp)
 
