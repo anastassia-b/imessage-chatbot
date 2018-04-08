74e1e0fe445d30dd8a666a092755b4dafc773593
Anastassia Bobokalonova <email.anastassia@gmail.com>
2018-04-08 13:49:38 -0700

    Create Side component

--- rnn-frontend/src/App.js
+++ rnn-frontend/src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import './stylesheets/app.css';
 import Chat from './components/Chat.js';
+import Side from './components/Side.js';
 
 class App extends Component {
   render() {
     return (
       <div className="app">
         <header className="app-side">
+          <Side />
         </header>
         <main className="app-main">
           <Chat />
---
+++ rnn-frontend/src/components/Side.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+class Side extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className="side">
+        <h4>About</h4>
+        <div>
+
+        </div>
+        <h4>Options</h4>
+        <div>
+
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Side;
